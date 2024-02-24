4bb747f20cdcc76a34256972571f7a93496a8cdf
mxc <3097893714@qq.com>
2024-02-24 23:01:19 +0800

    完成Tab高亮

--- src/sections/SecondSection/index.tsx
+++ src/sections/SecondSection/index.tsx
@@ -2,19 +2,49 @@ import CartoonImage from '../../assets/cartoon.jpg'
 import MoviceImage from '../../assets/movie.png'
 import LifeImage from '../../assets/life.jpg'
 import FoodImage from '../../assets/food.jpg'
+import classNames from 'classnames'
 
 import styles from './styles.module.scss'
+import { useState } from 'react'
+
+
+const tabs = [
+  {
+    key: 'cartoon',
+    title: '动画',
+  },
+  {
+    key: 'food',
+    title: '美食'
+  },
+  {
+    key: 'movie',
+    title: '电影'
+  },
+  {
+    key: 'life',
+    title: '生活'
+  }
+]
+
+
 
 
 const SecondSection = () => {
+
+  const [activeTab, setActiveTab] = useState<String>('cartoon')
   return (
     <div className={styles.secondSection}>
       {/* Tabs */}
       <ul>
-        <li>动画</li>
-        <li>美食</li>
-        <li>电影</li>
-        <li>生活</li>
+        {tabs.map(tab => (
+          <li key={tab.key} onClick={() => setActiveTab(tab.key)}>
+            <span>{tab.title}</span>
+            {/* <span className={''} /> ${activeTab === tab.key && 'visible'} 百度怎么写 */}
+            {/* {activeTab === tab.key && <span className={styles.line} />} */}
+            <span className={classNames(styles.line, activeTab === tab.key && styles.visible)} />
+          </li>
+        ))}
       </ul>
       {/* Tab Content */}
       <div>
